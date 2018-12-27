90b65c16e671b68e49941b914dfff73672b0f7bf
ginle20799 <leminhnhat20799@gmail.com>
2018-12-27 16:05:31 +0700

    done header

--- dist/js/main.js
+++ dist/js/main.js
@@ -31,4 +31,30 @@ $(function () {
         $('select.form-control').removeClass('form-control').css('width', '100%');
     }
 });
+$(document).ready(function () {
+    abc();
+    // $('body header .navbar ul li').removeClass('active')
+    // $('body header .navbar ul li a').on('click', function() {
+    //     $(this).parents('li').addClass('active')
+    //     console.log(123)
+    // })
+});
+$(window).scroll(function () {
+    abc();
+});
+
+function abc() {
+    var ac = $(window).scrollTop();
+    if (ac === 0) $('body header').removeClass('active');else if (ac > 0) $('body header').addClass('active');
+}
+var scrollToTop = window.setInterval(function () {
+    var pos = window.pageYOffset;
+    if (pos > 0) {
+        window.scrollTo(0, pos - 999);
+    } else {
+        window.clearInterval(scrollToTop);
+    }
+}, 16);
+
+function link() {}
 //# sourceMappingURL=main.js.map
