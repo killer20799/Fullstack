dab1b014ed1aa0179ee7b3cfd787d94c20202460
ginle20799 <leminhnhat20799@gmail.com>
2019-03-18 23:41:07 +0700

    fix reponsive

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,30 @@
 $(document).ready(function() {
-
-})
-$(window).scroll(function() {
-
-})
-$(window).resize(function() {
-
-})
+  
+	var descMinHeight = 120;
+	var desc = $('.desc');
+	var descWrapper = $('.desc-wrapper');
+  
+	// show more button if desc too long
+	if (desc.height() > descWrapper.height()) {
+	  $('.more-info').show();
+	}
+	
+	// When clicking more/less button
+	$('.more-info').click(function() {
+	  
+	  var fullHeight = $('.desc').height();
+  
+	  if ($(this).hasClass('expand')) {
+		// contract
+		$('.desc-wrapper').animate({'height': descMinHeight}, 'slow');
+	  }
+	  else {
+		// expand 
+		$('.desc-wrapper').css({'height': descMinHeight, 'max-height': 'none'}).animate({'height': fullHeight}, 'slow');
+	  }
+  
+	  $(this).toggleClass('expand');
+	  return false;
+	});
+  
+  });
